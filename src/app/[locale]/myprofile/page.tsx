fb9289e566ae68327dd77379545788a1d1a75427
vin0dkhichar <abbb61667@gmail.com>
2026-04-02 19:09:09 +0530

    G2P-4437 Remove static profile data

--- src/app/[locale]/myprofile/page.tsx
+++ src/app/[locale]/myprofile/page.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import LanguageSwitcher from '@/components/LanguageSwitcher';
+import { useAuth } from '@/context/Authcontext';
 import Image from 'next/image';
 
 
 export default function MyProfile() {
+    const { user } = useAuth()
+
     const profile = {
-        name: 'John Doe',
+        name: user?.name,
         position: 'Registry Officer',
         phone: '+91 98765 43210',
         email: 'john.doe@registry.com',
@@ -19,9 +22,10 @@ export default function MyProfile() {
         notifications: 'enabled',
         image: '/profile.png',
     };
+
     return (
-        <div className="min-h-screen bg-[#F3F1E4]">
-            <div className="flex mx-7.5 bg-white rounded-[10px]">
+        <div className="min-h-screen bg-[#F3F1E4] p-7.5">
+            <div className="flex bg-white rounded-[10px] min-h-150">
                 <div
                     className="w-[20%] bg-[#F2BA1A] bg-[url('/bg_pattern.png')] rounded-[10px] shadow-[0px_17px_21px_0px_#F2BA1A2B] flex items-start justify-center"
                 >
@@ -37,25 +41,25 @@ export default function MyProfile() {
                 <div className="w-[80%] flex-1 rounded-[30px] pt-10">
                     <div className="px-12.5 py-4">
                         <h1 className="text-[30px] font-semibold text-[#ED7C22]">{profile.name}</h1>
-                        <p className="text-[18px] font-semibold text-black">{profile.position}</p>
+                        {/* <p className="text-[18px] font-semibold text-black">{profile.position}</p> */}
                     </div>
 
-                    <div className="grid grid-cols-2 gap-6 px-12.5 py-4 bg-[#D9D9D940]">
+                    {/* <div className="grid grid-cols-2 gap-6 px-12.5 py-4 bg-[#D9D9D940]">
                         <Info label="Phone" value={profile.phone} verified />
                         <Info label="Email" value={profile.email} verified />
-                    </div>
+                    </div> */}
 
-                    <div className="grid grid-cols-2 gap-6 px-12.5 py-4">
+                    {/* <div className="grid grid-cols-2 gap-6 px-12.5 py-4">
                         <Info label="Location" value={profile.location} />
                         <Info label="Registry ID" value={profile.registryId} />
-                    </div>
+                    </div> */}
 
-                    <div className="grid grid-cols-2 gap-6 px-12.5 py-4 bg-[#D9D9D940]">
+                    {/* <div className="grid grid-cols-2 gap-6 px-12.5 py-4 bg-[#D9D9D940]">
                         <Info label="Gender" value={profile.gender} />
                         <Info label="Date of Birth" value={profile.dob} />
-                    </div>
+                    </div> */}
 
-                    <div className="grid grid-cols-2 gap-6 px-12.5 py-4">
+                    <div className="grid grid-cols-2 gap-6 px-12.5 py-4 bg-[#D9D9D940]">
                         <div>
                             <p className="text-sm text-gray-500 mb-1">Language</p>
                             <div className='-ml-4'>
@@ -63,7 +67,7 @@ export default function MyProfile() {
                             </div>
                         </div>
 
-                        <div>
+                        {/* <div>
                             <p className="text-[14px] text-black/50 mb-1">Timezone</p>
                             <div className="flex items-center justify-between">
                                 <p className="text-[18px] font-medium">{profile.timezone}</p>
@@ -75,10 +79,10 @@ export default function MyProfile() {
                                     className="cursor-pointer"
                                 />
                             </div>
-                        </div>
+                        </div> */}
                     </div>
 
-                    <div className="px-12.5 py-4 bg-[#D9D9D940]">
+                    {/* <div className="px-12.5 py-4 bg-[#D9D9D940]">
                         <p className="text-sm text-gray-500 mb-2">Notifications</p>
                         <div className="flex gap-6">
                             <label className="flex items-center gap-2">
@@ -99,11 +103,11 @@ export default function MyProfile() {
                                 Disabled
                             </label>
                         </div>
-                    </div>
-                    <div className="mt-auto pt-10 px-12.5 py-4 text-start">
+                    </div> */}
+                    {/* <div className="mt-auto pt-10 px-12.5 py-4 text-start">
                         <p className="text-[14px] text-black/50">Powered by</p>
                         <p className="text-[18px] text-black/50">OpenG2P Registry</p>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
